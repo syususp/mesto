a5a4b47ef4d9971fe643c7799627fb8cb58b2770
Nikita K <syususp@yandex.ru>
2023-01-26 00:10:46 +0300

    ADD JS + polish CSS

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup_form');
+let nameInput = document.querySelector('#popupInputTitle');
+let jobInput = document.querySelector('#popupInputSubtitle');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let jobInputValue; 
+let nameInputValue;
+
+function showPopup () {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    nameInputValue = nameInput.value;
+    jobInputValue = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    profileTitle.textContent = nameInputValue;
+    profileSubtitle.textContent = jobInputValue;
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit); 
+editButton.addEventListener('click', showPopup); 
+popupCloseButton.addEventListener('click', closePopup);
+
