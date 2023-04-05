57e81cea3e788454b54a833affc9e357a6f1d7f6
syususp <syususp@yandex.ru>
2023-04-06 02:29:22 +0300

    Add _id UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,6 +3,7 @@ export default class UserInfo {
     this._nameElement = document.querySelector(nameSelector);
     this._infoElement = document.querySelector(infoSelector);
     this._avatarElement = document.querySelector(avatarSelector);
+    this.userId = null;
   }
 
   getUserInfo() {
@@ -13,9 +14,10 @@ export default class UserInfo {
     };
   }
 
-  setUserInfo({ name, about, avatar }) {
+  setUserInfo({ name, about, avatar, _id }) {
     this._nameElement.textContent = name;
     this._infoElement.textContent = about;
     this._avatarElement.src = avatar;
+    this.userId = _id;
   }
 }
